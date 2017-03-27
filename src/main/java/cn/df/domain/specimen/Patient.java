aadd272bc2af2e61d8bf0b24756a7e9b264d7e0c
simagle <cao784137>
2017-03-27 12:53:48 +0800

    病人表调整

--- src/main/java/cn/df/domain/specimen/Patient.java
+++ src/main/java/cn/df/domain/specimen/Patient.java
@@ -11,8 +11,8 @@
 {  维护历史:													
 {  日期        维护人        维护类型						
 {  ---------------------------------------------------------------------------	
-{  2017-03-24  Katybaby        新建	
-{ 	                                                                     
+{  2017-03-27  Katybaby        新建
+{
 {*****************************************************************************
 */
 
@@ -31,19 +31,71 @@ import org.apache.commons.lang.builder.ToStringStyle;
  */
 public class Patient extends BaseDomain<Long> {
 	private static final long serialVersionUID = 1L;
-	
+
+	private String code; //病人代码，主键，为32位uuid
+	private String address; //住址
+	private String name; //姓名
+	private String career; //职业
 	private Integer gender; //性别，0：男，1：女
 	private Integer age; //年龄
-	private String code; //病人代码，主键，为32位uuid
+	private Double probability; //患病概率
 	private Integer isSicken; //是否患病，0：否，1：是
-    
+
 	/**
 	 *默认空构造函数
 	 */
 	public Patient() {
 		super();
 	}
-	 
+
+	/**
+	 * @return code 病人代码，主键，为32位uuid
+	 */
+	public String getCode(){
+		return this.code;
+	}
+	/**
+	 * @param code 病人代码，主键，为32位uuid
+	 */
+	public void setCode(String code){
+		this.code = code;
+	}
+	/**
+	 * @return address 住址
+	 */
+	public String getAddress(){
+		return this.address;
+	}
+	/**
+	 * @param address 住址
+	 */
+	public void setAddress(String address){
+		this.address = address;
+	}
+	/**
+	 * @return name 姓名
+	 */
+	public String getName(){
+		return this.name;
+	}
+	/**
+	 * @param name 姓名
+	 */
+	public void setName(String name){
+		this.name = name;
+	}
+	/**
+	 * @return career 职业
+	 */
+	public String getCareer(){
+		return this.career;
+	}
+	/**
+	 * @param career 职业
+	 */
+	public void setCareer(String career){
+		this.career = career;
+	}
 	/**
 	 * @return gender 性别，0：男，1：女
 	 */
@@ -69,16 +121,16 @@ public class Patient extends BaseDomain<Long> {
 		this.age = age;
 	}
 	/**
-	 * @return code 病人代码，主键，为32位uuid
+	 * @return probability 患病概率
 	 */
-	public String getCode(){
-		return this.code;
+	public Double getProbability(){
+		return this.probability;
 	}
 	/**
-	 * @param code 病人代码，主键，为32位uuid
+	 * @param probability 患病概率
 	 */
-	public void setCode(String code){
-		this.code = code;
+	public void setProbability(Double probability){
+		this.probability = probability;
 	}
 	/**
 	 * @return isSicken 是否患病，0：否，1：是
@@ -92,32 +144,42 @@ public class Patient extends BaseDomain<Long> {
 	public void setIsSicken(Integer isSicken){
 		this.isSicken = isSicken;
 	}
-	
+
 	public String toString() {
 		return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-			.append("id",getId())
-			.append("gender",getGender())
-			.append("age",getAge())
-			.append("code",getCode())
-			.append("isSicken",getIsSicken())
-			.toString();
+				.append("id",getId())
+				.append("code",getCode())
+				.append("address",getAddress())
+				.append("name",getName())
+				.append("career",getCareer())
+				.append("gender",getGender())
+				.append("age",getAge())
+				.append("probability",getProbability())
+				.append("isSicken",getIsSicken())
+				.toString();
 	}
-	
+
 	public int hashCode() {
 		return new HashCodeBuilder()
-			.append(getId())
-			.append(getGender())
-			.append(getAge())
-			.append(getCode())
-			.append(getIsSicken())
-			.toHashCode();
+				.append(getId())
+				.append(getCode())
+				.append(getAddress())
+				.append(getName())
+				.append(getCareer())
+				.append(getGender())
+				.append(getAge())
+				.append(getProbability())
+				.append(getIsSicken())
+				.toHashCode();
 	}
-	
+
 	public boolean equals(Object obj) {
 		if(obj instanceof Patient == false) return false;
 		if(this == obj) return true;
 		Patient other = (Patient)obj;
 		return new EqualsBuilder()
-			.isEquals();
+				.append(getId(),other.getId())
+				.append(getCode(),other.getCode())
+				.isEquals();
 	}
 }
--- src/main/java/cn/df/param/specimen/PatientParam.java
+++ src/main/java/cn/df/param/specimen/PatientParam.java
@@ -11,8 +11,8 @@
 {  维护历史:													
 {  日期        维护人        维护类型						
 {  ---------------------------------------------------------------------------	
-{  2017-03-24  Katybaby        新建	
-{ 	                                                                     
+{  2017-03-27  Katybaby        新建
+{
 {*****************************************************************************
 */
 
@@ -29,36 +29,104 @@ import org.apache.commons.lang.builder.ToStringStyle;
  */
 public class PatientParam extends BaseParam<Long> {
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
-	*字段常量——性别，0：男，1：女
-	*/
-	public static final String F_Gender="gender";
-	/**
-	*字段常量——年龄
-	*/
-	public static final String F_Age="age";
-	/**
-	*字段常量——病人代码，主键，为32位uuid
-	*/
+	 *字段常量——病人代码，主键，为32位uuid
+	 */
 	public static final String F_Code="code";
 	/**
-	*字段常量——是否患病，0：否，1：是
-	*/
+	 *字段常量——住址
+	 */
+	public static final String F_Address="address";
+	/**
+	 *字段常量——姓名
+	 */
+	public static final String F_Name="name";
+	/**
+	 *字段常量——职业
+	 */
+	public static final String F_Career="career";
+	/**
+	 *字段常量——性别，0：男，1：女
+	 */
+	public static final String F_Gender="gender";
+	/**
+	 *字段常量——年龄
+	 */
+	public static final String F_Age="age";
+	/**
+	 *字段常量——患病概率
+	 */
+	public static final String F_Probability="probability";
+	/**
+	 *字段常量——是否患病，0：否，1：是
+	 */
 	public static final String F_IsSicken="isSicken";
-	
+
+	private String code; //病人代码，主键，为32位uuid
+	private String address; //住址
+	private String name; //姓名
+	private String career; //职业
 	private Integer gender; //性别，0：男，1：女
 	private Integer age; //年龄
-	private String code; //病人代码，主键，为32位uuid
+	private Double probability; //患病概率
 	private Integer isSicken; //是否患病，0：否，1：是
-    
+
 	/**
 	 *默认空构造函数
 	 */
 	public PatientParam() {
 		super();
 	}
-	 
+
+	/**
+	 * @return code 病人代码，主键，为32位uuid
+	 */
+	public String getCode(){
+		return this.code;
+	}
+	/**
+	 * @param code 病人代码，主键，为32位uuid
+	 */
+	public void setCode(String code){
+		this.code = code;
+	}
+	/**
+	 * @return address 住址
+	 */
+	public String getAddress(){
+		return this.address;
+	}
+	/**
+	 * @param address 住址
+	 */
+	public void setAddress(String address){
+		this.address = address;
+	}
+	/**
+	 * @return name 姓名
+	 */
+	public String getName(){
+		return this.name;
+	}
+	/**
+	 * @param name 姓名
+	 */
+	public void setName(String name){
+		this.name = name;
+	}
+	/**
+	 * @return career 职业
+	 */
+	public String getCareer(){
+		return this.career;
+	}
+	/**
+	 * @param career 职业
+	 */
+	public void setCareer(String career){
+		this.career = career;
+	}
 	/**
 	 * @return gender 性别，0：男，1：女
 	 */
@@ -84,16 +152,16 @@ public class PatientParam extends BaseParam<Long> {
 		this.age = age;
 	}
 	/**
-	 * @return code 病人代码，主键，为32位uuid
+	 * @return probability 患病概率
 	 */
-	public String getCode(){
-		return this.code;
+	public Double getProbability(){
+		return this.probability;
 	}
 	/**
-	 * @param code 病人代码，主键，为32位uuid
+	 * @param probability 患病概率
 	 */
-	public void setCode(String code){
-		this.code = code;
+	public void setProbability(Double probability){
+		this.probability = probability;
 	}
 	/**
 	 * @return isSicken 是否患病，0：否，1：是
@@ -107,15 +175,19 @@ public class PatientParam extends BaseParam<Long> {
 	public void setIsSicken(Integer isSicken){
 		this.isSicken = isSicken;
 	}
-	
+
 	public String toString() {
 		return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-			.append("id",getId())
-			.append("gender",getGender())
-			.append("age",getAge())
-			.append("code",getCode())
-			.append("isSicken",getIsSicken())
-			.toString();
+				.append("id",getId())
+				.append("code",getCode())
+				.append("address",getAddress())
+				.append("name",getName())
+				.append("career",getCareer())
+				.append("gender",getGender())
+				.append("age",getAge())
+				.append("probability",getProbability())
+				.append("isSicken",getIsSicken())
+				.toString();
 	}
-	
+
 }
